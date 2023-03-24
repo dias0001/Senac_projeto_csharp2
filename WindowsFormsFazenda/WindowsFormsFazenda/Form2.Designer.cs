1e66bda837c7fdeab999f9d9613b56b2a36d0cb8
Luiz Gustavo Chaves dias <l.g.chaves.d@gmail.com>
2023-03-24 11:38:18 -0300

    new update

---
+++ WindowsFormsFazenda/WindowsFormsFazenda/Form2.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsFazenda
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.farmDataSet = new WindowsFormsFazenda.farmDataSet();
+            this.materiaisBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.materiaisTableAdapter = new WindowsFormsFazenda.farmDataSetTableAdapters.materiaisTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.farmDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.materiaisBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.materiaisBindingSource, "mat_Nm", true));
+            this.textBox1.Location = new System.Drawing.Point(163, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.materiaisBindingSource, "mat_Prof", true));
+            this.textBox2.Location = new System.Drawing.Point(163, 162);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(179, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.materiaisBindingSource, "mat_dias_da_semana", true));
+            this.textBox3.Location = new System.Drawing.Point(163, 193);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(179, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Profissão";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Dias da semana";
+            // 
+            // farmDataSet
+            // 
+            this.farmDataSet.DataSetName = "farmDataSet";
+            this.farmDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // materiaisBindingSource
+            // 
+            this.materiaisBindingSource.DataMember = "materiais";
+            this.materiaisBindingSource.DataSource = this.farmDataSet;
+            // 
+            // materiaisTableAdapter
+            // 
+            this.materiaisTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.ControlBox = false;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "Cadastro materias";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.farmDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.materiaisBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private farmDataSet farmDataSet;
+        private System.Windows.Forms.BindingSource materiaisBindingSource;
+        private farmDataSetTableAdapters.materiaisTableAdapter materiaisTableAdapter;
+    }
+}
---
+++ WindowsFormsFazenda/WindowsFormsFazenda/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsFazenda
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'farmDataSet.materiais'. Você pode movê-la ou removê-la conforme necessário.
+            this.materiaisTableAdapter.Fill(this.farmDataSet.materiais);
+
+        }
+    }
+}
